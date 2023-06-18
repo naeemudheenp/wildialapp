de3be81eba10ed98db4611ecc8d33f52eb825a80
naeemudheenp-surveysparrow <nayeemudheenaslam3@gmail.com>
2023-06-18 09:30:00 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  AwesomeNotifications().initialize(
-    // set the icon to null if you want to use the default app icon
-      'resource://drawable/res_app_icon',
-      [
-        NotificationChannel(
-            channelGroupKey: 'basic_channel_group',
-            channelKey: 'basic_channel',
-            channelName: 'Basic notifications',
-            channelDescription: 'Notification channel for basic tests',
-            defaultColor: Color(0xFF9D50DD),
-            ledColor: Colors.white)
-      ],
-      // Channel groups are only visual and are not required
-      channelGroups: [
-        NotificationChannelGroup(
-            channelGroupKey: 'basic_channel_group',
-            channelGroupName: 'Basic group')
-      ],
-      debug: true
-  );
+
 
 
   runApp(const MyApp());
